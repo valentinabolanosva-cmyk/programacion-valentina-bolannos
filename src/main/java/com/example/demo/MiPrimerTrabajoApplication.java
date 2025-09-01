2313dbe3a5f563e3fff0cada8d65b1ab08a76b6c
Valentina Bolaños <valentina_bolanosva@fet.edu.co>
2025-09-01 10:55:51 -0500

    agregamos una lista

--- src/main/java/com/example/demo/MiPrimerTrabajoApplication.java
+++ src/main/java/com/example/demo/MiPrimerTrabajoApplication.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class MiPrimerTrabajoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiPrimerTrabajoApplication.class, args);
 	}
+        ArrayList<String> nombres = new ArrayList<>();
+
 
 }
