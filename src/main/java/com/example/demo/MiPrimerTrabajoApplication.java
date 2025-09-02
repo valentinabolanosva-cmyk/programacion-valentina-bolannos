4fc07173897bee01dab8c70f4e9279929ba0dd50
Valentina Bolaños <valentina_bolanosva@fet.edu.co>
2025-09-02 09:50:29 -0500

    Quitar la lista nombres

--- src/main/java/com/example/demo/MiPrimerTrabajoApplication.java
+++ src/main/java/com/example/demo/MiPrimerTrabajoApplication.java
@@ -11,7 +11,7 @@ public class MiPrimerTrabajoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MiPrimerTrabajoApplication.class, args);
 	}
-        ArrayList<String> nombres = new ArrayList<>();
+
 
 
 }
